df819eac8b8f04849fc76262c024ed1abd9c363e
Yoshida sasuga <amrkn21@yahoo.co.jp>
2015-11-04 12:51:09 +0900

    ユーザ登録機能の追加

---
+++ WebContent/user_login.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="Login_servlet"method="POST">
+ユーザネーム<input type="text" name="user_name"><br>
+パスワード<input type="text" name="user_pass"><br>
+<input type="submit" name="送信"value="ログイン"><br>
+
+</form>
+</body>
+</html>
---
+++ src/Login_test/Login_servlet.java
@@ -0,0 +1,52 @@
+package Login_test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login_servlet
+ */
+@WebServlet("/Login_servlet")
+public class Login_servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login_servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//文字コードの設定
+		request.setCharacterEncoding("utf-8");
+
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+
+
+
+		request.setAttribute("name", name);
+		request.setAttribute("pass", pass);
+		request.getRequestDispatcher("login_result.jsp").forward(request, response);
+
+	}
+
+}
