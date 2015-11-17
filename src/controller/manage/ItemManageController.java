4818ea2ab1cb06aea4e11ed872ecd2a8bb90413d
Yamaguchi Yuta <Yuta@example.com>
2015-11-17 18:26:37 +0900

    ItemManageControllerの修正

--- src/controller/manage/ItemManageController.java
+++ src/controller/manage/ItemManageController.java
@@ -8,10 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import DAO.ItemsDAO;
 import model.Item;
 import util.Router;
 import util.Util;
+import DAO.ItemsDAO;
 
 /**
  * Servlet implementation class ItemManageController
@@ -41,61 +41,57 @@ public class ItemManageController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	  String item_id = Util.getUUID();
-	  System.out.println(item_id);
-	  String category_id = request.getParameter("category_id");
-	   String name = request.getParameter("name");
-	    String strprice = request.getParameter("price");
-	    String strsize = request.getParameter("size");
-	    String info = request.getParameter("info");
-	    String strstock = request.getParameter("stock");
-	    String exterior_path = request.getParameter("ex_image");
-	    int is_alive = 1;
-	    int price =0;
-	    int size =0;
-	    int stock =0;
+	  String item_id         = Util.getUUID();
+	  String category_id     = request.getParameter("category_id");
+	  String name            = request.getParameter("name");
+	  String strprice        = request.getParameter("price");
+	  String strsize         = request.getParameter("size");
+	  String info            = request.getParameter("info");
+	  String strstock        = request.getParameter("stock");
+	  String exterior_path   = request.getParameter("ex_image");
+	  int is_alive           = 1;
+	  int price              = 0;
+	  int size               = 0;
+	  int stock              = 0;
+	  String nextJsp         = "";
+	  boolean isError       = false;
 
 	    //////////////////////////////////
 	    //////////////////////////////////
 	    //ログイン→ショップ管理画面→ここで自動取得したい
-	    String shop_id = "9e64ca309b2743fd83d3c7aff2f5a55f";
+	  String shop_id = "9e64ca309b2743fd83d3c7aff2f5a55f";
 
 	    /////////////////////////////////////
 	    //////////////////////////////////////
-	    //
 	    //////////////////////////////////
 
 
-	    String date = Util.getCurrentDateString();
-	    System.out.println(date);
+	  String date = Util.getCurrentDateString();
+	  System.out.println(date);
 
-	    try{
-	      price = Integer.parseInt(strprice);
-	      size = Integer.parseInt(strsize);
-	      stock = Integer.parseInt(strstock);
-
-
-	    }catch(NumberFormatException e){
-	      request.setAttribute("numErr", "価格、サイズ、在庫は数値で入力してください。");
-	      request.getRequestDispatcher( Router.getInstance().getURL( "formShopData.jsp" )).forward(request, response);
-
-	    }
-
-Item item = new Item(item_id,category_id,name,price,size,info,stock,exterior_path,is_alive,date);
-Item shop_item = new Item(shop_id,item_id,date);
-
-ItemsDAO dao = new ItemsDAO();
-ItemsDAO dao2 = new ItemsDAO();
-
-dao.create(item);
-
-dao.createShopItem(shop_item);
-
-request.setAttribute("MS","ok。");
-
-request.getRequestDispatcher( Router.getInstance().getURL( "ItemRegist.jsp" )).forward(request, response);
+	  try{
+	    price = Integer.parseInt(strprice);
+	    size = Integer.parseInt(strsize);
+	    stock = Integer.parseInt(strstock);
 
 
+	  }catch(NumberFormatException e){
+	    request.setAttribute("numErr", "価格、サイズ、在庫は数値で入力してください。");
+	    nextJsp = "formShopData.jsp";
+	    isError = true;
+	  }
+
+	  Item item      = new Item(item_id,category_id,name,price,size,info,stock,exterior_path,is_alive,date);
+	  Item shop_item = new Item(shop_id,item_id,date);
+	  ItemsDAO dao   = new ItemsDAO();
+
+	  dao.create(item);
+	  dao.createShopItem(shop_item);
+	  if ( isError ) { nextJsp = "formShopData.jsp";  }
+	  else { nextJsp = "ItemRegist.jsp";
+	         request.setAttribute("MS","ok。");
+	  }
+
+	  request.getRequestDispatcher( Router.getInstance().getURL( nextJsp )).forward(request, response);
 	}
-
 }
