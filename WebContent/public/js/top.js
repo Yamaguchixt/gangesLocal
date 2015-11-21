546c18bca896c4049509eed4bb92fd2d13b192d7
Yoshida sasuga <amrkn21@yahoo.co.jp>
2015-11-22 00:45:29 +0900

    商品登録に画像追加機能追加

---
+++ WebContent/public/js/top.js
@@ -0,0 +1,88 @@
+﻿   var GuideSentence = 'メールアドレスを入力';
+   function ShowFormGuide(obj) {
+      // 入力案内を表示
+      if( obj.value == '' ) {
+         obj.value = GuideSentence;
+         obj.style.color = '#808080';
+      }
+   }
+   function HideFormGuide(obj) {
+      // 入力案内を消す
+      if( obj.value == GuideSentence ) {
+         obj.value='';
+         obj.style.color = '#000000';
+      }
+   }
+
+
+
+   var GuideSentence2 = 'パスワードを入力';
+   function ShowFormGuide2(obj) {
+      // 入力案内を表示
+      if( obj.value == '' ) {
+         obj.value = GuideSentence2;
+         obj.style.color = '#808080';
+      }
+   }
+   function HideFormGuide2(obj) {
+      // 入力案内を消す
+      if( obj.value == GuideSentence2 ) {
+         obj.value='';
+         obj.style.color = '#000000';
+      }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+<script type="text/javascript">
+   var GuideSentence = 'メールアドレスを入力';
+   function ShowFormGuide(obj) {
+      // 入力案内を表示
+      if( obj.value == '' ) {
+         obj.value = GuideSentence;
+         obj.style.color = '#808080';
+      }
+   }
+   function HideFormGuide(obj) {
+      // 入力案内を消す
+      if( obj.value == GuideSentence ) {
+         obj.value='';
+         obj.style.color = '#000000';
+      }
+   }
+
+
+
+   var GuideSentence2 = 'パスワードを入力';
+   function ShowFormGuide2(obj) {
+      // 入力案内を表示
+      if( obj.value == '' ) {
+         obj.value = GuideSentence2;
+         obj.style.color = '#808080';
+      }
+   }
+   function HideFormGuide2(obj) {
+      // 入力案内を消す
+      if( obj.value == GuideSentence2 ) {
+         obj.value='';
+         obj.style.color = '#000000';
+      }
+   }
+
+</script>
+
+
+*/
